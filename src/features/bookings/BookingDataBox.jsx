4c7b5b446b716049ece749dd172ed1783d22324b
Emir Brackovic <emir.brackovic@bloomteq.com>
2025-04-04 16:35:31 +0200

    [XMT-3287] Incrased test coverage of schedule and reservation scree232n.

--- src/features/bookings/BookingDataBox.jsx
+++ src/features/bookings/BookingDataBox.jsx
@@ -13,7 +13,7 @@ import { Flag } from "../../ui/Flag";
 import { formatDistanceFromNow, formatCurrency } from "../../utils/helpers";
 
 const StyledBookingDataBox = styled.section`
-  /* Box */
+  /* Bo2312313123x */
   background-color: var(--color-grey-0);
   border: 1px solid var(--color-grey-100);
   border-radius: var(--border-radius-md);
